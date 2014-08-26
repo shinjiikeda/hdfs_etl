a7416ac363f03e7f90ba57b1fdfd31e985b8fc97
Shinji Ikeda <ikeda.shinji@dena.jp>
2014-08-27 03:45:31 +0900

    update

---
+++ bin/hdfs_etl.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env jruby
+
+require 'kafka_etl/hdfs'
+require 'optparse'
+
+
+zookeeper = 'localhost:2181/hdfs_etl'
+kafka_brokers = ["localhost:9092"]
+kafka_topic_name = "hdfs_etl_queue"
+hdfs_prefix = "./test_etl"
+
+kafka_client_id="hdfs_etl_consumer"
+kafka_topic_part_num = 64
+
+max_fetch_bytes = 50 * 1024 * 1024
+num_threads     = 4
+
+opt = OptionParser.new
+opt.on('--zk zookeeper') {|v| zookeeper = v }
+opt.on('--topic kafka_topic_name') {|v| kafka_topic_name = v}
+opt.on('--kafka_brokers kafka_brokers') {|v| kafka_brokers = v}
+opt.on('--hdfs_prefix hdfs_prefix_path') {|v| hdfs_prefix_path = v}
+opt.on('--kafka_client_id kafka_client_id') { |v| kafka_client_id = v}
+opt.on('--daemon') { |v| is_daemon = v}
+
+Process.daemon if is_daemon
+
+etl = KafkaETL::Hdfs.new(zookeeper, kafka_brokers, kafka_topic_name, hdfs_prefix,
+                         :kafka_client_id => kafka_client_id,
+                         :kafka_topic_part_num => kafka_topic_part_num,
+                         :max_fetch_bytes => max_fetch_bytes,
+                         :num_threads => num_threads
+                         )
+
+loop do
+  etl.procsss
+  sleep 5
+end
+
