c8626c50e66f0b1dc8128e9d8c0622ed21926547
Shinji Ikeda <ikeda.shinji@dena.jp>
2014-09-27 02:16:55 +0900

    update

--- bin/hdfs_etl.rb
+++ bin/hdfs_etl.rb
@@ -2,7 +2,7 @@
 
 require 'kafka_etl/hdfs'
 require 'optparse'
-
+require 'logger'
 
 zookeeper = 'localhost:2181/hdfs_etl'
 kafka_brokers = ["localhost:9092"]
@@ -15,6 +15,7 @@ kafka_topic_part_num = 4
 max_fetch_bytes = 50 * 1024 * 1024
 num_threads     = 4
 is_daemon = false
+logfile = nil
 
 opt = OptionParser.new
 opt.on('--daemon') {|v| is_daemon = v }
@@ -23,7 +24,15 @@ opt.on('--topic kafka_topic_name') {|v| kafka_topic_name = v}
 opt.on('--kafka_brokers kafka_brokers') {|v| kafka_brokers = v}
 opt.on('--hdfs_prefix hdfs_prefix_path') {|v| hdfs_prefix_path = v}
 opt.on('--kafka_client_id kafka_client_id') { |v| kafka_client_id = v}
+opt.on('--logfile path') {|v| logfile = v}
 opt.parse!(ARGV)
+
+if logfile.nil?
+  $log = Logger.new(STDOUT)
+else
+  $log = Logger.new(logfile, shift_age = 10, shift_size = 10485760)
+end
+$log.level = Logger::INFO
 
 $exit_process = false
 Signal.trap(:TERM, proc{ $exit_process = true })
