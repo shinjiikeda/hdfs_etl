9732703d65cad065bd4ad7535d735512f16348d5
scaleout <so@batch101.(none)>
2016-03-22 14:38:19 +0900

    update

--- lib/hdfs_etl.rb
+++ lib/hdfs_etl.rb
@@ -90,7 +90,8 @@ module HdfsETL
               $log.info("part: #{part_no}, path: #{path} size: #{current_filesize} success")
               is_success = true
               break
-            rescue org.apache.hadoop.hdfs.protocol.AlreadyBeingCreatedException => e
+            rescue org.apache.hadoop.ipc.RemoteException, org.apache.hadoop.hdfs.protocol.AlreadyBeingCreatedException => e
+            #rescue org.apache.hadoop.hdfs.protocol.AlreadyBeingCreatedException => e
               $log.error("path: #{path} failure! broken file")
               _dir =  File.dirname(path)
               broken_dir = sprintf("%s/lost+found/%s", @hdfs_prefix, _dir)
